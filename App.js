1c5bde5f6d22525fdb45f994a395a09a1b8cc8d9
Jonny Burger <jonathanburger11@gmail.com>
2019-06-24 18:47:28 +0200

    Add instructions

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ export default function App() {
 	return (
 		<View style={styles.container}>
 			{/**
+				Start the app first with the line commented
+				Then enable Live reload
+				and uncomment the line below.
+			*/}
+			{/**
 			<Image source={require('../../Assets/img.png')} />
 			 */}
 			<Text>Open up App.js to start working on your app!</Text>
